1b6168be4f792195d2abcfbebbf7176b49bf0308
Al Ekram Hossain Abir <alabir65@gmail.com>
2020-08-23 21:50:02 +0600

    MongoDB added

--- config/datastores.js
+++ config/datastores.js
@@ -47,5 +47,7 @@ module.exports.datastores = {
      ***************************************************************************/
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
+    adapter: "sails-mongo",
+    url: "mongodb://root@localhost/counsil",
   },
 };
